6bba433bb79c388740437e29ac0310dc584ee8d9
Scott Corgan <scottcorgan@gmail.com>
2015-10-03 18:08:47 -0700

    format list of values

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var _ = require('lodash');
 var feedback = require('feedback');
 var chalk = require('chalk');
 var util = require('util');
+var asArray = require('as-array');
 
 var print = module.exports = function (data, options) {
   if (isBasicArray(data)) return printBasicArray(data, options);
@@ -16,36 +17,47 @@ function printObject (data, options) {
     leftPadding: 2,
     rightPadding: 2
   };
-  
+
   _.defaults(options, defaults);
-  
+
   var keys = _.keys(data);
   var maxKeyLen = _.max(_.map(keys, function (key) {
     return key.length;
   })) + options.rightPadding;
-  
+
   _.each(keys, function (key) {
     var paddedKey = addPadding(key, maxKeyLen);
     var leftPadding = options.leftPadding;
     var lPad = '';
-    
+
     while (leftPadding--) {
       lPad += ' ';
     }
-    
+
     if (util.isArray(data[key]) && data[key].length > 0){
       var blankKey = key.replace(/./gi, ' '); // used for spacing
       var blankPaddedKey = addPadding(blankKey, maxKeyLen);
       var arr = data[key];
-      
+
       feedback.info(lPad + paddedKey + JSON.stringify(arr[0]).replace(/^\"|\"$/g, ''));
-      
+
       for(var i = 1; i < arr.length; i += 1) {
         feedback.info(lPad + blankPaddedKey + JSON.stringify(arr[i]).replace(/^\"|\"$/g, ''));
       }
     }
     else{
-      feedback.info(lPad + paddedKey + JSON.stringify(data[key]).replace(/^\"|\"$/g, ''));
+
+    	var output = Array.isArray(data[key])
+	    	? asArray(data[key])
+		    	.map(function (item) {
+
+		    		return JSON.stringify(item).replace(/^\"|\"$/g, '');
+		    	})
+		    	.join('')
+    		: JSON.stringify(data[key]).replace(/^\"|\"$/g, '');
+
+
+      feedback.info(lPad + paddedKey + output);
     }
   });
 }
@@ -56,26 +68,26 @@ function printArrayOfObjects (data, options) {
     leftPadding: 2,
     rightPadding: 2
   };
-  
+
   _.defaults(options, defaults);
-  
+
   var maxKeyLen = _.max(_.map(data, function (obj) {
     return obj[options.key].length;
   })) + options.rightPadding;
-  
+
   _.each(data, function (obj) {
     var objKey = obj[options.key];
     var leftPadding = options.leftPadding;
     var lPad = '';
-    
+
     if (!objKey) return;
-    
+
     while (leftPadding--) {
       lPad += ' ';
     }
-    
+
     var paddedKey = lPad + addPadding(objKey, maxKeyLen);
-    
+
     if (options.value) paddedKey += obj[options.value];
     feedback.info(paddedKey);
   });
@@ -85,16 +97,16 @@ function printBasicArray (data, options) {
   var defaults = {
     leftPadding: 2
   };
-  
+
   var leftPadding = options.leftPadding;
   var lPad = '';
-  
+
   while (leftPadding--) {
     lPad += ' ';
   }
-  
+
   _.defaults(options, defaults);
-  
+
   _.each(data, function (val) {
     feedback.info(lPad + val);
   });
